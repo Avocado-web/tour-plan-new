062e7aae04556f79113b98f40e13529d7c7bdfbc
Avocado-web <igorahparov@gmail.com>
2021-07-22 19:26:01 +0300

    create a new repository

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const hotelSlider = new Swiper('.hotel-slider', {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+  keyboard: {
+    enabled: true,
+  },
+  effect: "coverflow",
+});
+
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+  keyboard: {
+    enabled: true,
+  },
+});
+
+
+
